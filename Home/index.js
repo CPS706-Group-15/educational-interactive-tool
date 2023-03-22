8a1594aa35a206aeca6535d620e955994eb81156
Akshara Debnath <70068533+akshxrx@users.noreply.github.com>
2023-03-21 20:37:30 -0400

    commit1

---
+++ Home/index.js
@@ -0,0 +1,6 @@
+const responsive = document.querySelector('.responsivePic');
+const nav = document.querySelector('.nav__link');
+
+function swapper() {
+    toggleClass(document.getElementById('nav__link'), 'hide');
+}
