ef8b12f8225c669597161b16142d419409ab9830
Kara Huynh <karahuynh122@gmail.com>
2023-04-09 17:46:08 -0400

    animations implemented (must be debugged)

--- Decentralized/distance-vector.js
+++ Decentralized/distance-vector.js
@@ -22,41 +22,44 @@ class Graph {
     n2.connections.set(router1, distance);
   }
 
-  getShortestPaths(startRouter) {
+  
+  getShortestPaths(startRouter){
 
-    // gsap.registerEffect({
-    //   name: "swapText",
-    //   effect: (targets, config) => {
-    //     let tl = gsap.timeline({delay: config.delay});
-    //     tl.to(targets, {opacity: 0, duration: config.duration / 2});
-    //     tl.add(() => targets[0].innerHTML = config.text);
-    //     tl.to(targets, {opacity: 1, duration: config.duration});
-    //     return tl;
-    //   },
-    //   defaults: {duration: 1}, 
-    //   extendTimeline: true
-    // });
+    gsap.registerEffect({
+      name: "swapText",
+      effect: (targets, config) => {
+        let tl = gsap.timeline({delay: config.delay});
+        tl.to(targets, {opacity: 0, duration: config.duration / 2});
+        tl.add(() => targets[0].innerHTML = config.text);
+        tl.to(targets, {opacity: 1, duration: config.duration});
+        return tl;
+      },
+      defaults: {duration: 1}, 
+      extendTimeline: true
+    });
 
-    // gsap.registerEffect({
-    //   name: "text",
-    //   effect: (targets, config) => {
-    //     let tl = gsap.timeline({delay: config.delay});
-    //     tl.to(targets, {opacity: 0, duration: config.duration / 2});
-    //     tl.add(() => targets[0].innerHTML = config.text);
-    //     tl.to(targets, {opacity: 1, duration: config.duration});
-    //     return tl;
-    //   },
-    //   defaults: {duration: 0}, 
-    //   extendTimeline: true
-    // });
+    gsap.registerEffect({
+      name: "text",
+      effect: (targets, config) => {
+        let tl = gsap.timeline({delay: config.delay});
+        tl.to(targets, {opacity: 0, duration: config.duration / 2});
+        tl.add(() => targets[0].innerHTML = config.text);
+        tl.to(targets, {opacity: 1, duration: config.duration});
+        return tl;
+      },
+      defaults: {duration: 0}, 
+      extendTimeline: true
+    });
 
-    // gsap.effects.swapText('#TDV'+ startRouter, {text:'0'});
-    // gsap.effects.swapText('#TPV' + startRouter, {text:'0'});
-    // gsap.effects.swapText("#SourceNode" , {text: startRouter });
 
     let distances = new Map();
     let visited = new Set();
     let queue = [startRouter];
+    const parent = {};
+
+    gsap.effects.swapText('#TDV'+ startRouter.name ,{text:'0'});
+    gsap.effects.swapText('#TPV' + startRouter.name ,{text:'0'});
+    gsap.effects.swapText("#SourceNode" , {text: startRouter.name });
   
     // Initialize distances to Infinity for all routers except startRouter and unconnected routers
     for (let router of this.routers.values()) {
@@ -66,30 +69,94 @@ class Graph {
         distances.set(router.name, Infinity);
       }
     }
-  
-    while (queue.length > 0) {
-      let currentrouter = queue.shift();
-      visited.add(currentrouter);
-  
-      // Update distances to neighboring routers
-      for (let [routerName, distance] of currentrouter.connections) {
-        let router = this.routers.get(routerName);
-        if (!visited.has(router)) {
-          let newDistance = distances.get(currentrouter.name) + distance;
-          if (newDistance < distances.get(routerName)) {
-            distances.set(routerName, newDistance);
-          }
-          if (!queue.includes(router)) {
-            queue.push(router);
-          }
-        }
+
+    for (let router of this.routers.values()) {
+      parent[router.name] = 0;
+    };
+
+    const firstloop = async () => {
+      while (queue.length > 0) {
+        let currentrouter = queue.shift();
+        visited.add(currentrouter);
+        
+        // Update distances to neighboring routers
+        const doSomething = async () => {
+          for (let [routerName, distance] of currentrouter.connections) {
+            let router = this.routers.get(routerName);
+            if (!visited.has(router)) {
+              let newDistance = distances.get(currentrouter.name) + distance;
+
+              gsap.effects.text("#current-node" ,{text:currentrouter.name});
+              gsap.effects.swapText("#neighbor-node",{text:routerName});
+              gsap.effects.swapText("#currentNode" ,{text:currentrouter.name}); 
+              gsap.effects.swapText("#neighborNode" ,{text:routerName});
+              gsap.effects.swapText("#calculating-distance" ,{text: newDistance});
+
+              let calcuate_distance;
+              if(distances.get(routerName) == Infinity && newDistance != Infinity){
+                 calcuate_distance = newDistance + ' < ' + '&#x221e <b style="color:crimson;"> [update!]</b>';
+              } else if(newDistance < distances.get(routerName)){
+                calcuate_distance = newDistance  + ' < ' + distances.get(routerName) + '<b style="color:crimson;"> [update!]</b>';
+              } else{
+                calcuate_distance = newDistance + ' < ' + distances.get(routerName)
+              }
+              
+              gsap.effects.swapText("#calculating-distance2" ,{text: calcuate_distance });
+              
+              if (newDistance < distances.get(routerName)) {
+                let elements;
+                let vector;
+                
+                if((parent[routerName] != 0)){
+                  console.log("here",routerName,parent[routerName]);
+                  //console.log("yess");
+                  elements = document.getElementsByClassName(routerName, parent[routerName]);
+                  if (elements.length > 0) {  
+                    vector = '.' + routerName + parent[routerName];
+                   } else {
+                    vector = '.'  + parent[routerName]  + routerName;
+                  }
+                  gsap.to(vector, {duration: 2, stroke: '#00527F'});
+    
+                }
+    
+                parent[routerName] = currentrouter.name;
+                distances.set(routerName, newDistance);
+                  
+                elements = document.getElementsByClassName(routerName +currentrouter.name);
+    
+                if (elements.length > 0) {  
+                  vector = '.' + routerName + currentrouter.name;
+                } else {
+                  vector = '.'  +currentrouter.name + routerName;
+                }
+      
+                gsap.to(vector, {duration: 2, stroke: '#800000'});
+                gsap.effects.swapText('#TDV'+ routerName, {text:newDistance});
+                gsap.effects.swapText('#TPV' + routerName, {text:currentrouter.name});
+          
+              }
+
+              if (!queue.includes(router)) {
+                queue.push(router);
+              }
+            }
+    
+            await sleep(1000);
+          }// for loop end
+    
+        }// do something end
+        doSomething();
+        await sleep(7000);
       }
-    }
-  
+      gsap.effects.swapText("#algo-finished" ,{text: '<b style="color:crimson; text-shadow: 2px 4px 4px rgba(46,91,173,0.6);"> Algorithm Completed!</b>' });
+    }//firstloop end
+    firstloop();
     return distances;
-  }
+  } // end function
  
-  
+
+
 }
 
 // Create a new graph
@@ -123,6 +190,8 @@ graph.addConnection("D", "E", 4);
 graph.addConnection("D", "F", 6);
 
 graph.addConnection("E", "F", 7);
+
+console.log(graph);
 
 function submit() {
   console.log("ABC");
@@ -166,4 +235,12 @@ function submit() {
     }
   }
 
+}
+
+const sleep = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+
+function refreshPage(){
+  window.location.reload();
 }
